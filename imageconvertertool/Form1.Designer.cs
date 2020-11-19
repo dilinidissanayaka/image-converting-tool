c84835135969cfb211ea1b4c122ffd5440e8349f
dilini <dilinidissanayak18@gmail.com>
2020-11-19 14:10:10 +0530

    first commit

---
+++ imageconvertertool/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace imageconvertertool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.green = new System.Windows.Forms.Button();
+            this.blue = new System.Windows.Forms.Button();
+            this.redimage = new System.Windows.Forms.Button();
+            this.negative = new System.Windows.Forms.Button();
+            this.gray = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.save = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.binary = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.binary);
+            this.splitContainer1.Panel1.Controls.Add(this.green);
+            this.splitContainer1.Panel1.Controls.Add(this.blue);
+            this.splitContainer1.Panel1.Controls.Add(this.redimage);
+            this.splitContainer1.Panel1.Controls.Add(this.negative);
+            this.splitContainer1.Panel1.Controls.Add(this.gray);
+            this.splitContainer1.Panel1.Controls.Add(this.open);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.save);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(574, 406);
+            this.splitContainer1.SplitterDistance = 299;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // green
+            // 
+            this.green.Location = new System.Drawing.Point(0, 274);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(75, 23);
+            this.green.TabIndex = 6;
+            this.green.Text = "greenimage";
+            this.green.UseVisualStyleBackColor = true;
+            this.green.Click += new System.EventHandler(this.green_Click);
+            // 
+            // blue
+            // 
+            this.blue.Location = new System.Drawing.Point(0, 245);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(75, 23);
+            this.blue.TabIndex = 5;
+            this.blue.Text = "blueimage";
+            this.blue.UseVisualStyleBackColor = true;
+            this.blue.Click += new System.EventHandler(this.blue_Click);
+            // 
+            // redimage
+            // 
+            this.redimage.Location = new System.Drawing.Point(-2, 216);
+            this.redimage.Name = "redimage";
+            this.redimage.Size = new System.Drawing.Size(75, 23);
+            this.redimage.TabIndex = 4;
+            this.redimage.Text = "redimage";
+            this.redimage.UseVisualStyleBackColor = true;
+            this.redimage.Click += new System.EventHandler(this.redimage_Click);
+            // 
+            // negative
+            // 
+            this.negative.Location = new System.Drawing.Point(0, 187);
+            this.negative.Name = "negative";
+            this.negative.Size = new System.Drawing.Size(75, 23);
+            this.negative.TabIndex = 3;
+            this.negative.Text = "negative";
+            this.negative.UseVisualStyleBackColor = true;
+            this.negative.Click += new System.EventHandler(this.negative_Click);
+            // 
+            // gray
+            // 
+            this.gray.Location = new System.Drawing.Point(-2, 148);
+            this.gray.Name = "gray";
+            this.gray.Size = new System.Drawing.Size(75, 33);
+            this.gray.TabIndex = 2;
+            this.gray.Text = "grayscale";
+            this.gray.UseVisualStyleBackColor = true;
+            this.gray.Click += new System.EventHandler(this.gray_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(37, 23);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 1;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(108, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(30, 23);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 2;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(63, 118);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image File |*.png;*.jpg;*.bmp";
+            this.openFileDialog1.Title = "Open An Image";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Png File|*.png";
+            this.saveFileDialog1.Title = "save the image";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // binary
+            // 
+            this.binary.Location = new System.Drawing.Point(0, 303);
+            this.binary.Name = "binary";
+            this.binary.Size = new System.Drawing.Size(75, 23);
+            this.binary.TabIndex = 7;
+            this.binary.Text = "binary image";
+            this.binary.UseVisualStyleBackColor = true;
+            this.binary.Click += new System.EventHandler(this.binary_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 406);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button gray;
+        private System.Windows.Forms.Button green;
+        private System.Windows.Forms.Button blue;
+        private System.Windows.Forms.Button redimage;
+        private System.Windows.Forms.Button negative;
+        private System.Windows.Forms.Button binary;
+    }
+}
+
---
+++ imageconvertertool/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageconvertertool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox2.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void gray_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap newimg = new Bitmap(img.Width, img.Height);
+
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color c = img.GetPixel(i, j);
+                    int gray = (int)((c.R * 0.2) + (c.G * 0.5) + (c.B * 0.2));
+                    Color newC = Color.FromArgb(c.A, gray, gray, gray);
+                    newimg.SetPixel(i, j, newC);
+
+                }
+            }
+            pictureBox2.Image = newimg;
+
+        }
+
+        private void negative_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            int width = img.Width;
+            int height = img.Height;
+
+
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color c = img.GetPixel(i, j);
+                    int a = c.A;
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+
+                    img.SetPixel(i, j, Color.FromArgb(a, r, g, b));
+
+
+                }
+            }
+
+            pictureBox2.Image = img;
+
+        }
+
+        private void redimage_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap newimg = new Bitmap(img.Width, img.Height);
+
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color c = img.GetPixel(i, j);
+
+                    Color newr = Color.FromArgb(c.A, c.R, 0, 0);
+
+                    newimg.SetPixel(i, j, newr);
+                }
+                pictureBox2.Image = newimg;
+
+
+            }
+
+        }
+
+        private void green_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap newimg = new Bitmap(img.Width, img.Height);
+
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color c = img.GetPixel(i, j);
+
+                    Color newr = Color.FromArgb(c.A, 0, c.G, 0);
+
+                    newimg.SetPixel(i, j, newr);
+
+                }
+            }
+            pictureBox2.Image = newimg;
+
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap newimg = new Bitmap(img.Width, img.Height);
+
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color c = img.GetPixel(i, j);
+
+                    Color newr = Color.FromArgb(c.A, 0, 0, c.B);
+
+                    newimg.SetPixel(i, j, newr);
+
+                }
+            }
+            pictureBox2.Image = newimg;
+
+        }
+
+        private void binary_Click(object sender, EventArgs e)
+        {
+
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap newimg = new Bitmap(img.Width, img.Height);
+            string texto = "";
+
+
+ 
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+              
+        if (img.GetPixel(j, i).A.ToString() == "255" 
+                        && img.GetPixel(j, i).B.ToString() == "255" 
+                        && img.GetPixel(j, i).G.ToString() == "255"
+                        && img.GetPixel(j, i).R.ToString() == "255")
+                    {
+                        texto = texto + "0";
+                    }
+                    else
+                    {
+                        texto = texto + "1";
+                    }
+                }
+                texto = texto + "\r\n";   
+            }
+
+            pictureBox2.Image = newimg;
+
+
+
+        }
+    }
+}
